5ea1bf074dd032bd06eedd24915522065ce277f9
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:31:12 +0530

    .

---
+++ P9.java
@@ -0,0 +1,17 @@
+public class P9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+            System.out.println(4/0);
+        }catch(ArithmeticException e)
+        {
+            System.out.println("ArithmeticException : divide by 0");
+        }catch(Exception e)
+        {
+            System.out.println("Exception : divide by 0");
+        }
+
+	}
+
+}

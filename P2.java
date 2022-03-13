d483d2cc1931c1516a79fe8465deef4121fb39f2
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:22:50 +0530

    .

---
+++ P2.java
@@ -0,0 +1,13 @@
+public class P2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int a=10;
+				for(int i=3;i>=0;i--)
+				   try{
+				     System.out.println(a/i);  
+				   }catch(ArithmeticException e){
+				       System.out.println(e);
+				   }
+			}
+		}

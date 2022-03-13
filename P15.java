e8196cef68017efa9cbf5de898bd7a614637e090
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:40:10 +0530

    .

---
+++ P15.java
@@ -0,0 +1,17 @@
+public class P15 {
+	static void func(int a) throws Exception{
+		   System.out.println(10/a);  
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+		    func(10);
+		    func(0);
+		}catch(Exception e){
+		   System.out.println("can't divide by zero");
+		}
+	
+	}
+
+}

3e642b69df1808bb17ed6bab0bf32be491bdee5f
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:37:50 +0530

    .

---
+++ P13.java
@@ -0,0 +1,15 @@
+public class P13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 try{
+	            System.out.println(4/0);
+	            
+	        }finally
+	        {
+	            System.out.println("cleaning.......");
+	        }
+
+	}
+
+}

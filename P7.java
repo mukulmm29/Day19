af4cab60d28ea772201a1689d92829010394779a
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:27:14 +0530

    .

---
+++ P7.java
@@ -0,0 +1,16 @@
+public class P7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        try{
+	            System.out.println(4/0);
+	           }
+	      
+	        //superclass of ArithmeticException 
+	        catch(Exception e){
+	            System.out.println("divide by the 0");
+	        }
+
+	}
+
+}

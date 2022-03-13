27d42dac8853cc41670cd52a6d2d5f430bef9761
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:23:38 +0530

    .

---
+++ P3.java
@@ -0,0 +1,17 @@
+public class P3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		for(int i=3;i>=0;i--) 
+		{
+		     try{
+				   System.out.println(a/i);  
+			    }
+			 catch(ArithmeticException e)
+		         {
+				    System.out.println(e);
+				 }
+			}
+		}
+}

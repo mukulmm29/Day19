54707934c52a47dc223ad17ff37fdf550efbb8c9
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:26:11 +0530

    Create P6.java

---
+++ P6.java
@@ -0,0 +1,16 @@
+public class P6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 try{
+	            System.out.println(4/0);
+	           }
+	      
+	        //ArithmeticException 
+	        catch(ArithmeticException e){
+	            System.out.println("divide by 0");
+	        }
+
+	}
+
+}

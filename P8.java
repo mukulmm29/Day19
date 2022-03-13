fcd15420a453997770db336535c8c2272d54612e
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:29:13 +0530

    .

---
+++ P8.java
@@ -0,0 +1,15 @@
+public class P8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  try{
+	            System.out.println(4/0);
+	        }
+		  catch(Exception e)
+	        {
+	            System.out.println("Exception : divide by 0");
+	        }
+
+	}
+
+}

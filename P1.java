bc34d7c3bb09b182b5b8e1ed1a787bf166c69a7d
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-13 19:21:49 +0530

    .

---
+++ P1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  try {
+		        System.out.println(5/0);
+		      }
+		  catch(ArithmeticException e)
+	        {
+			  System.out.println("End of program!");	        }
+		       
+			}
+		}
